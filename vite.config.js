66a71cae9f062ec02eca9d0731899652edf8d67e
Vladyslav <vladsw764@icloud.com>
2023-07-26 14:10:22 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/minty-cleaning",
+  base: "/minty-front-2",
 });
